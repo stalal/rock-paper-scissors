127a50b6b5304f03e3450072302187d3018e8a59
Talal Syed <3298457+stalal@users.noreply.github.com>
2023-08-23 23:31:58 -0400

    Added function for computer choice

--- app.js
+++ app.js
@@ -21,22 +21,53 @@ function game(){
   let computerChoice = getComputerChoice();
   let playerChoice = getPlayerChoice();
 
-  // use a loop and run it 5 times 
+  // use a loop and run it 5 times
   let resultOfRound = gameRound(playerChoice, computerChoice);
-  document.getElementsByClassName("para")[0].textContent = resultOfRound;
+  document.getElementsByClassName("roundResult")[0].textContent = resultOfRound;
 
 }
+
+// Rock beats scissors
+// Scissors beats paper
+// Paper beats rock 
 
 function gameRound(playerChoice, computerChoice){
   if(playerChoice == computerChoice){
     return "The computer also chose " + computerChoice + ". It's a draw!"
   }
+
+  else if (playerChoice == "Rock" && computerChoice == "Paper"){
+    return "You lose, Paper beats Rock!"
+  }
+  
+  else if (playerChoice == "Rock" && computerChoice == "Scissors"){
+    return "You win, Rock beats Scissors!"
+  }
+
+  else if (playerChoice == "Paper" && computerChoice == "Rock"){
+    return "You win, Paper beats Rock!"
+  }
+  
+  else if (playerChoice == "Paper" && computerChoice == "Scissors"){
+    return "You lose, Scissors beats Paper!"
+  }
+  
+  else if (playerChoice == "Scissors" && computerChoice == "Rock"){
+    return "You lose, Rock beats Scissors!"
+  }
+  
+  else if (playerChoice == "Scissors" && computerChoice == "Paper"){
+    return "You win, Scissors beats Paper!"
+  }
+
 }
 
 function getComputerChoice(){
-  return "Rock";
+  let choices = ["Rock", "Paper", "Scissors"];
+  let randomIndex = Math.floor(Math.random()*3)+1;
+  return choices[randomIndex];
 }
 
 function getPlayerChoice(){
-  return "Rock";
+  return "Paper";
 }
